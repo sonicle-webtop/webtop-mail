893c4eae72828ac9c7b8d03d72517027c50dabc8
Gabriele Bulfon <gbulfon@sonicle.com>
2016-02-09 18:08:26 +0100

    override tree viewdropzone handleOnDrop to workaround session bug and implement different drop models (folders, emails)

--- src/main/client/sonicle/webtop/mail/Service.js
+++ src/main/client/sonicle/webtop/mail/Service.js
@@ -145,13 +145,15 @@ Ext.define('Sonicle.webtop.mail.Service', {
 					//enabledDrag: false,
 					containerScroll: true,
 					dropZone: {
-						
+						handleNodeDrop : function(data, targetNode, position) {
+							me.imapTreeNodeDrop(this,data,targetNode,position);
+						}
 					}
 				},
 				markDirty: false,
 				
 				//workaround for bug in handleNodeDrop function of Ext.tree.ViewDropZone
-				listeners: {
+				/*listeners: {
 					beforedrop: function(node, data, overModel, dropPosition, dropHandlers) {
 						var record = data.records[0];
 
@@ -178,7 +180,7 @@ Ext.define('Sonicle.webtop.mail.Service', {
 
 						this.droppedRecords = undefined;
 					}
-				}			
+				}*/			
 				
 			},
 
@@ -629,6 +631,131 @@ Ext.define('Sonicle.webtop.mail.Service', {
 	
 	getFolderTrash: function() {
 		return this.getOption('folderTrash');
-	}
+	},
 	
+    imapTreeNodeDrop : function(dz, data, targetNode, position) {
+        var targetView = dz.view,
+            parentNode = targetNode ? targetNode.parentNode : targetView.panel.getRootNode(),
+            Model = targetView.store.getModel(),
+            records, i, len, record,
+            insertionMethod, argList,
+            needTargetExpand,
+            transferData;
+
+        // If the copy flag is set, create a copy of the models
+        if (data.copy) {
+            records = data.records;
+            data.records = [];
+            for (i = 0, len = records.length; i < len; i++) {
+                record = records[i];
+                if (record.isNode) {
+                    data.records.push(record.copy());
+                } else {
+                    // If it's not a node, make a node copy
+                    data.records.push(new Model(Ext.apply({}, record.data)));
+                }
+            }
+        }
+
+        // Cancel any pending expand operation
+        dz.cancelExpand();
+
+        // Grab a reference to the correct node insertion method.
+        // Create an arg list array intended for the apply method of the
+        // chosen node insertion method.
+        // Ensure the target object for the method is referenced by 'targetNode'
+        if (position === 'before') {
+            insertionMethod = parentNode.insertBefore;
+            argList = [null, targetNode];
+            targetNode = parentNode;
+        }
+        else if (position === 'after') {
+            if (targetNode.nextSibling) {
+                insertionMethod = parentNode.insertBefore;
+                argList = [null, targetNode.nextSibling];
+            }
+            else {
+                insertionMethod = parentNode.appendChild;
+                argList = [null];
+            }
+            targetNode = parentNode;
+        }
+        else {
+            if (!(targetNode.isExpanded() || targetNode.isLoading())) {
+                needTargetExpand = true;
+            }
+            insertionMethod = targetNode.appendChild;
+            argList = [null];
+        }
+        
+        // A function to transfer the data into the destination tree
+        transferData = function() {
+            var color,
+                n;
+
+            // Coalesce layouts caused by node removal, appending and sorting
+            Ext.suspendLayouts();
+
+            // Insert the records into the target node
+            for (i = 0, len = data.records.length; i < len; i++) {
+                record = data.records[i];
+                if (!record.isNode) {
+                    if (record.isModel) {
+                        //record = new Model(record.data, record.getId());
+						record = new Model(record.data, record.session);
+                    } else {
+                        record = new Model(record);
+                    }
+                    data.records[i] = record;
+                }
+                argList[0] = record;
+                insertionMethod.apply(targetNode, argList);
+
+                // Focus the dropped node.
+                targetView.getNavigationModel().setPosition(record);
+            }
+
+            // If configured to sort on drop, do it according to the TreeStore's comparator
+            if (dz.sortOnDrop) {
+                targetNode.sort(targetNode.getOwnerTree().store.getSorters().sortFn);
+            }
+            
+            Ext.resumeLayouts(true);
+
+            // Kick off highlights after everything's been inserted, so they are
+            // more in sync without insertion/render overhead.
+            // Element.highlight can handle highlighting table nodes.
+            if (Ext.enableFx && dz.dropHighlight) {
+                color = dz.dropHighlightColor;
+
+                for (i = 0; i < len; i++) {
+                    n = targetView.getNode(data.records[i]);
+                    if (n) {
+                        Ext.fly(n).highlight(color);
+                    }
+                }
+            }
+        };
+
+        // If dropping right on an unexpanded node, transfer the data after it is expanded.
+        if (needTargetExpand) {
+            targetNode.expand(false, transferData);
+        }
+        // If the node is waiting for its children, we must transfer the data after the expansion.
+        // The expand event does NOT signal UI expansion, it is the SIGNAL for UI expansion.
+        // It's listened for by the NodeStore on the root node. Which means that listeners on the target
+        // node get notified BEFORE UI expansion. So we need a delay.
+        // TODO: Refactor NodeInterface.expand/collapse to notify its owning tree directly when it needs to expand/collapse.
+        else if (targetNode.isLoading()) {
+            targetNode.on({
+                expand: transferData,
+                delay: 1,
+                single: true
+            });
+        }
+        // Otherwise, call the data transfer function immediately
+        else {
+            transferData();
+        }
+    }    
 });
