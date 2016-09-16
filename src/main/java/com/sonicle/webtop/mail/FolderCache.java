d1e01dd37a1d8d805cd132dfad32554137bc1a33
Gabriele Bulfon <gbulfon@sonicle.com>
2016-09-16 08:34:11 +0200

    threading and paging start/limit calculations

--- src/main/java/com/sonicle/webtop/mail/FolderCache.java
+++ src/main/java/com/sonicle/webtop/mail/FolderCache.java
@@ -1696,15 +1696,19 @@ public class FolderCache {
         return mailData;
     }
 	
-	public void setThreadOpen(long uid, boolean open) throws MessagingException {
+	public synchronized void setThreadOpen(long uid, boolean open) throws MessagingException {
 		int children=((SonicleIMAPMessage)getMessage(uid)).getThreadChildren();
 		if (open) {
-			totalOpenThreadChildren+=children;
-			openThreads.put(uid, children);
+			if (!openThreads.containsKey(uid)) {
+				totalOpenThreadChildren+=children;
+				openThreads.put(uid, children);
+			}
 		}
 		else {
-			totalOpenThreadChildren-=children;
-			openThreads.remove(uid);
+			if (openThreads.containsKey(uid)) {
+				totalOpenThreadChildren-=children;
+				openThreads.remove(uid);
+			}
 		}
 	}
 	
@@ -1714,7 +1718,8 @@ public class FolderCache {
 	}
 	
 	public int getThreadedCount() {
-		return ((SonicleIMAPFolder)folder).getThreadRoots()+totalOpenThreadChildren;
+		int threadRoots=((SonicleIMAPFolder)folder).getThreadRoots();
+		return threadRoots+totalOpenThreadChildren;
 	}
     
     public ArrayList<String> getHTMLParts(MimeMessage m, long msguid, boolean forEdit) throws MessagingException, IOException {
